ba9d67d40594da22596c4d67e01c73bed4291851
Parzival <ayushrex1509@gmail.com>
2023-10-25 17:34:16 +0530

    Complete image upload functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -5,5 +5,6 @@ import { signup, signin, google } from '../controllers/auth.controller.js';
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.post('/google', google);
+// router.get('/signout', signOut);
 
 export default router;
