3221dcf4543cd85db0d19dc53f4e8ee839db69e4
Parzival <ayushrex1509@gmail.com>
2023-10-25 15:49:56 +0530

    Update Firebase account details

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,13 +4,24 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// // Baijnath's Firebase
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "mern-estate-920cf.firebaseapp.com",
+//   projectId: "mern-estate-920cf",
+//   storageBucket: "mern-estate-920cf.appspot.com",
+//   messagingSenderId: "427398761886",
+//   appId: "1:427398761886:web:03c1bf98ccadbe2bfd5a8f"
+// };
+
+// Parzival's Firebase
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-estate-920cf.firebaseapp.com",
-  projectId: "mern-estate-920cf",
-  storageBucket: "mern-estate-920cf.appspot.com",
-  messagingSenderId: "427398761886",
-  appId: "1:427398761886:web:03c1bf98ccadbe2bfd5a8f"
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: 'mern-estate-b24ee.firebaseapp.com',
+	projectId: 'mern-estate-b24ee',
+	storageBucket: 'mern-estate-b24ee.appspot.com',
+	messagingSenderId: '245954369054',
+	appId: '1:245954369054:web:30849a142260bd2fb6800f',
 };
 
 // Initialize Firebase
