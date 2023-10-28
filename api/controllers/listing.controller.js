baec667f04f48d446d44a88e7c114e5d14535ac3
Parzival <ayushrex1509@gmail.com>
2023-10-28 22:48:48 +0530

    Create update listing API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -16,7 +16,7 @@ export const deleteListing = async (req, res, next) => {
 	if (!listing) {
 		return next(errorHandler(404, 'Listing not found!'));
 	}
-	
+
 	if (req.user.id !== listing.userRef.toString()) {
 		return next(errorHandler(401, 'You can only delete your own listings!'));
 	}
@@ -27,4 +27,26 @@ export const deleteListing = async (req, res, next) => {
 	} catch (error) {
 		next(error);
 	}
+};
+
+export const updateListing = async (req, res, next) => {
+	const listing = await Listing.findById(req.params.id);
+
+	if (!listing) {
+		return next(errorHandler(404, 'Listing not found!'));
+	}
+
+	if (req.params.id !== listing.userRef) {
+		return next(401, 'You can only update your own listing!');
+	}
+
+	try {
+		const updateListing = await Listing.findByIdAndUpdate(
+			req.params.id,
+			req.body,
+			{ new: true }
+		);
+
+		res.status(200).json(updateListing);
+	} catch (error) {}
 };
